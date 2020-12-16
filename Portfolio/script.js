fc22bf91b5cfb51c3f121d5bbf5d47abc3c24f0a
Clara Hester <clarahester@Claras-MacBook-Air.local>
2020-12-15 20:53:28 -0600

    12-15-20

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,9 +1,33 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon*/
 function myFunction() {
-    var x = document.getElementById("navbar");
+    let x = document.getElementById("navbar");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
       x.className = "topnav";
     }
   }
+
+
+  window.addEventListener("scroll", () => {
+    let isNavtop = window.scrollY>100 
+    let navbar = document.getElementById("navbar")
+    let divNavlinks = document.getElementById("divNavlinks");
+    if (isNavtop) {
+      navbar.className += " horizontal";
+    } else {
+      navbar.className = " topnav";
+    }
+  });
+
+
+const navLink = document.getElementsByClassName("navLink");
+
+for (var i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
